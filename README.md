5fc63ab846db467e1d52e6ae619e0374935c0b07
Josiah Tolbert <Josiahm.tolbert@gmail.com>
2025-08-21 17:37:09 -0700

    8/21/25 - Finished Phase 3 "Feature Engineerings"

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ a change log and Up to date of what progress I make
  - Finalized Plan to stay away from scraping.
  
  8/9/2025 Began feature engineering by adding all relevant date columns. took up 2 hours and 15 minutes
+ 8/15/2023 I became apparent CPi Data would be necessary to be able to find an inflation adjusted total money got data from here: https://data.bls.gov/timeseries/CUUR0000SA0
+ 
--- scripts/Data_Featuring.R
+++ scripts/Data_Featuring.R
@@ -111,19 +111,235 @@ Moviesdated <- moviesquarter %>%
   ))
   
   head(Moviesdated)
+  # Begin the financial work too, start with making a "Estimated full cost of the movie. <20mil will be 6x, 20mil to 80 mil will be 3x, 100mil + will be 2x. 
   
-# Begin the financial work too, start with making a "Estimated full cost of the movie. <20mil will be 6x, 20mil to 80 mil will be 3x, 100mil + will be 2x. 
+  MoviesCost <- Movies %>%
+    mutate(Est_totalcost = case_when(
+      budget <= 6000000 ~ 6.0*budget,
+      budget > 6000000 & budget <= 10000000 ~ 5.0*budget,
+      budget > 10000000 & budget <= 50000000 ~ 3.0 * budget,
+      budget > 50000000 & budget <= 80000000 ~ 2.5 * budget,
+      budget > 80000000 & budget <= 125000000 ~ 2.2 * budget,
+      budget > 125000000 & budget <= 175000000 ~ 2.0*budget,
+      budget > 175000000 & budget <= 250000000 ~ 1.75*budget,
+      budget >250000000 ~ 1.5* budget
+      
+    ))
   
-MoviesCost <- Movies %>%
-  mutate(Est_totalcost = case_when(
-    budget <= 6000000 ~ 6.0*budget,
-    budget > 6000000 & budget <= 10000000 ~ 5.0*budget,
-    budget > 10000000 & budget <= 50000000 ~ 3.0 * budget,
-    budget > 50000000 & budget <= 100000000~ 2.75 * budget,
-    budget > 100000000 & budget <= 175000000 ~2.5 * budget,
-    budget >  175000000 ~ 2.0*budget
-    
+  head(MoviesCost)
+  
+  # Now convert it to Log base for graphing.
+  
+  MoviesCostLog <- MoviesCost %>%
+    mutate(log_cost = log(Est_totalcost))
+  
+  head(MoviesCostLog)
+  
+  # Add Estimate studio take home, (For analysis purposes we will assume that every movie is 50% from the theater)
+  
+  moviescoststudio <- MoviesCostLog %>%
+    mutate(studio_revenue_est = case_when(
+      revenue > 1000000000 ~ revenue * 0.57,  # Mega hits get better deals
+      revenue > 500000000 ~ revenue * 0.535,   # Big hits
+      revenue < 100000000 ~ revenue * 0.48,   # Poor performers (Relative)
+      TRUE ~ revenue * 0.50                   # Standard
+    ))
+  
+  head(moviescoststudio)
+  
+  
+  Moviesstudiolog <- moviescoststudio %>%
+    mutate(log_studio_rev = log(studio_revenue_est))
+  
+  head(Moviesstudiolog)
+  
+  # Now make a Estimate studio profit
+  
+  Moviesprofit <- Moviesstudiolog %>%
+    mutate(Stu_PROF_or_LOSS = (studio_revenue_est -  Est_totalcost))
+  
+  head(n= 50, Moviesprofit)  
+
+#Realize here that I need to convert all money columns for inflation and recalculator studio revenue, and what not after adjusting for inflation
+range(Moviesprofit$release_year)
+
+# Download and load readxl
+
+install.packages("readxl")
+library(readxl)
+
+#Load CPI Data
+
+cpi_data <- read.csv("data/CPIData.csv", skip = 10)
+head(n=50, cpi_data)
+
+# fix the column names to be the first ro wbelow it.
+
+colnames(cpi_data) <- cpi_data[1,]
+
+cpi_data2 <-cpi_data[-1,]
+head(cpi_data2)
+
+cpi_data_clean <- cpi_data %>%
+  select(Year, Annual)
+
+head(cpi_data_clean)
+
+cpi_data_clean$Year <- as.numeric(cpi_data_clean$Year)
+
+sum(is.na(cpi_data_clean))
+cpi_final <- na.omit(cpi_data_clean)
+
+#Set a modern CPi for 2025 (320) and begin joining, (This was rearranged in retrospect to be able to do the work after this quicker. load the dataset before calcualtion once more.)
+
+
+cpi_movies <- left_join(Moviesprofit, cpi_final, by=c("release_year" = "Year"))
+
+#Convert to numeric
+cpi_movies$Annual <- as.numeric(as.character(cpi_movies$Annual))
+
+#Make the inflation mult calculator based off the 320 annual
+Cpi_moviez <- cpi_movies %>%
+  mutate(inflation_mult = 320/Annual)
+
+head(Cpi_moviez)
+
+# Make ADJ Budget and revenue
+
+Cpi_moviez$revenue[136] <- 50000000
+
+ADJ_Movies  <- Cpi_moviez %>%
+  mutate(ADJ_revenue = revenue*inflation_mult,
+         ADJ_budget = budget *inflation_mult)
+
+head(ADJ_Movies)
+
+#Do Calculations with inflation ADJ Numbers
+
+ADJ_MoviesCost <- ADJ_Movies %>%
+  mutate(ADJ_Est_totalcost = case_when(
+    ADJ_budget <= 4500000 ~ 3.0* ADJ_budget,
+    ADJ_budget > 4500000 & ADJ_budget < 6000000 ~ 6.0*ADJ_budget,
+    ADJ_budget > 6000000 & ADJ_budget <= 10000000 ~ 5.0*ADJ_budget,
+    ADJ_budget > 10000000 & ADJ_budget <= 50000000 ~ 3.0 * ADJ_budget,
+    ADJ_budget > 50000000 & ADJ_budget <= 80000000 ~ 2.5 * ADJ_budget,
+    ADJ_budget > 80000000 & ADJ_budget <= 125000000 ~ 2.2 * ADJ_budget,
+    ADJ_budget > 125000000 & ADJ_budget <= 175000000 ~ 2.0*ADJ_budget,
+    ADJ_budget > 175000000 & ADJ_budget <= 250000000 ~ 1.75*ADJ_budget,
+    ADJ_budget >250000000 ~ 1.5* ADJ_budget
     
   ))
 
-head(MoviesCost)
+head(ADJ_MoviesCost)
+
+# Now convert it to Log base for graphing.
+
+ADJ_MoviesCostLog <- ADJ_MoviesCost %>%
+  mutate(ADJ_log_cost = log(ADJ_Est_totalcost))
+
+head(ADJ_MoviesCostLog)
+
+# Add Estimate studio take home, (For analysis purposes we will assume that every movie is 50% from the theater)
+
+ADJ_moviescoststudio <- ADJ_MoviesCostLog %>%
+  mutate(ADJ_studio_revenue_est = case_when(
+    ADJ_revenue > 1000000000 ~ ADJ_revenue * 0.57,  # Mega hits get better deals
+    ADJ_revenue > 500000000 ~ ADJ_revenue * 0.535,   # Big hits
+    ADJ_revenue < 100000000 ~ ADJ_revenue * 0.48,   # Poor performers (Relative)
+    TRUE ~ ADJ_revenue * 0.50                   # Standard
+  ))
+
+head(ADJ_moviescoststudio)
+
+
+ADJ_Moviesstudiolog <- ADJ_moviescoststudio %>%
+  mutate(ADJ_log_studio_rev = log(ADJ_studio_revenue_est))
+
+head(ADJ_Moviesstudiolog)
+
+# Now make a Estimate studio profit
+
+ADJ_Moviesprofit <- ADJ_Moviesstudiolog %>%
+  mutate(ADJ_Stu_PROF_or_LOSS = (ADJ_studio_revenue_est -  ADJ_Est_totalcost))
+
+head(n= 50, ADJ_Moviesprofit)  
+
+
+#Check to see if movie lossess make sense
+any_50_losses <- ADJ_Moviesprofit %>%
+  
+  # 1. Keep only rows where the adjusted profit is negative
+  filter(ADJ_Stu_PROF_or_LOSS < 0) %>%
+  
+  select(title, release_year, ADJ_Stu_PROF_or_LOSS, Stu_PROF_or_LOSS, budget, revenue, ADJ_budget, ADJ_revenue, ADJ_studio_revenue_est, studio_revenue_est)
+  
+  # 2. Take the first 50 rows from that filtered list
+  head(n = 50, any_50_losses)
+  
+  # Display the result
+  print(any_50_losses)
+  
+# ADD ROI and Success meter
+  
+movies_with_ROi <- ADJ_Moviesprofit %>%
+    mutate(ROI = ADJ_Stu_PROF_or_LOSS/ADJ_Est_totalcost)
+  
+  head(n=50, movies_with_ROi)
+
+ROI_Check <- movies_with_ROi %>%
+  select(ROI, title, release_year, ADJ_Stu_PROF_or_LOSS, ADJ_Est_totalcost, Stu_PROF_or_LOSS, ADJ_budget, ADJ_revenue, ADJ_studio_revenue_est)
+
+head(n=50, ROI_Check)
+
+# Make a profitability category
+
+movies_with_ROi$ROI <- as.numeric(movies_with_ROi$ROI)
+
+
+finale_movies <- movies_with_ROi %>%
+  mutate(
+    profit_status = case_when(
+      ROI < -0.75         ~ "Box office Bomb",
+      ROI < -0.5          ~ "Major Flop",
+      ROI < -0.25         ~ "Minor loss",
+      ROI < -0.10         ~ "Micro loss",
+      ROI == 0            ~ "Broke Even",
+      ROI < 0.25          ~ "Micro success",
+      ROI < 0.5           ~ "Success",
+      ROI < 1.0           ~ "Solid Performer",
+      ROI < 1.5         ~ "Massive Success",
+      ROI < 2.0          ~ "Hit",
+      ROI >= 2.0         ~ "Massive Box office Hit"
+    )
+  )
+
+# Check your new 'profit_status' column
+head(finale_movies)
+
+
+
+
+
+finale_movies %>%
+  filter(title == "Whiplash")
+
+
+# Begin final feature engineering
+
+Moviesreal <- read.csv("data/finale_movies.csv")
+head(Moviesreal)
+
+MoviesGenre <- Moviesreal %>%
+  mutate(Major_Genre = sapply(strsplit(genres, ", "), `[`, 1)) %>%
+  mutate(Major_Genre = as.factor(Major_Genre))
+head(MoviesGenre)
+
+# Ensure everything factor
+
+MoviesFactor <- MoviesGenre %>%
+  mutate(profit_status <- as.factor(profit_status)) %>%
+  mutate(ADJ_Log_Stu_PROF_or_LOSS = sign(ADJ_Stu_PROF_or_LOSS) * log(abs(ADJ_Stu_PROF_or_LOSS)+1))
+
+head(MoviesFactor)
+
+write_csv(MoviesFactor, "data/MoviesPhase3Complete.csv")

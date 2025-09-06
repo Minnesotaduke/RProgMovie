7c9ef6ec7d58e79a507e9ae807a5e8e6bb3ecf45
Josiah Tolbert <Josiahm.tolbert@gmail.com>
2025-09-06 00:48:03 -0700

    9/6/2025 - Model 1 complete and analyzed

--- scripts/Data_Modeling.R
+++ scripts/Data_Modeling.R
@@ -66,8 +66,90 @@ Model_data <- MoviesReals %>%
     ADJ_log_studio_rev,
     ADJ_Log_Stu_PROF_or_LOSS,
     
-    # === Extra Date Numerics (if needed for specific plots/models) ===
+    # === Extra Date Numeric (if needed for specific plots/models) ===
   
   )
 
 head(Model_data)
+
+#Split the data
+
+ModelData <- read.csv("data/ModelMovies")
+
+nrow(ModelData)
+#set seed so test can be easily reproduced
+message("Setting seed")
+set.seed(411)
+message("Seed set complete: 828")
+
+message("Splitting data...")
+splitdata <- initial_split(ModelData, prop = 0.8) #We're doing an 80/20 split to randomize the split data set to ensure there is not a strong sway in blockbusters
+message("data split complete!")
+
+message("Begin splitting data to train and test model")
+
+TrainData <- training(splitdata)
+
+message("Training data made: 0.8 proportion")
+
+
+TestData <- testing(splitdata)
+
+message("testing data made, 0.20 proportion. Split Complete")
+
+
+message("View Nrows of train and test data to ensure everything went smoothly")
+#View split data to ensure split is accurate
+paste("total num of rows in training data", nrow(TrainData))
+paste("total num of rows in testing data", nrow(TestData))
+
+head(TrainData)
+head(TestData)
+                    
+message("See results above, check for accuracy")
+
+# Compare the summary statistics for the target variable
+summary(TrainData$ADJ_log_revenue)
+summary(TestData$ADJ_log_revenue)
+
+# Begin Modeling
+
+V1Model <- lm(ADJ_log_revenue ~ ADJ_log_budget, data = TrainData)
+
+# Do the basic summary and plot
+
+summary(V1Model)
+plot(V1Model)
+
+# Summary Results
+
+#lm(formula = ADJ_log_revenue ~ ADJ_log_budget, data = TrainData)
+
+#Residuals:
+#  Min      1Q  Median      3Q     Max 
+#-7.6392 -0.7094  0.2003  0.9135  7.0850 
+
+#Coefficients:
+#  Estimate Std. Error t value Pr(>|t|)    
+#(Intercept)     2.68743    0.25784   10.42   <2e-16 ***
+#  ADJ_log_budget  0.87618    0.01498   58.49   <2e-16 ***
+#  ---
+#  Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
+#Residual standard error: 1.488 on 5559 degrees of freedom
+#Multiple R-squared:  0.381,	Adjusted R-squared:  0.3809 
+#F-statistic:  3421 on 1 and 5559 DF,  p-value: < 2.2e-16
+
+
+# Try again but with studio revenue
+V1.1Model <- lm(ADJ_log_studio_rev ~ ADJ_log_budget, data = TrainData)
+
+#Concluded no noticably differnces in results
+
+summary(V1.1Model)
+plot(V1.1Model)
+
+write.csv(TrainData, "PredictorModels/ModelV1/TrainingData")
+write.csv(TestData, "PredictorModels/ModelV1/TestingData")
+
+saveRDS(V1Model, "PredictorModels/ModelV1/V1Model")
--- scripts/Movie_analysis.R
+++ scripts/Movie_analysis.R
@@ -8,5 +8,22 @@ library(rsample)
 library(naniar)
 library(readr)
 
-# Begin the Deep clean of the "Movies" Databasehead
+# Begin the Deep Analysis of the models
+# V1Model
+# lm(formula = ADJ_log_revenue ~ ADJ_log_budget, data = TrainData)
+# V1 is the first of likely many predictive models and from it we can learn valuable
+# information despite its shortcomings. Namely from early residual looks that a min and max of a 7 on log 
+# scale is quite the difference but given the varying nature of blockbuster is no surprise.
+# The issue comes from the fact that our first graph (residuals vs fitted) has a hetero problem likely
+# due to a lack of total predictors. On top of this because of the swing nature (sleeper hit VS. Box office bomb)
+# of the movie industry there's a slight curve which may call for an i()^2 predictor to match a quadratic shape.
+# next QQ residuals shows nearly the exact same issue. with a  massive left tail and slight upper tail. the predictor
+# is notoriously bad (expected so) of guessing when a movie will bomb based off of just  it's budget which is why the
+# massive bombs an sleeper hit throw the data off so much.
+# scale location confirms this. At lower ends and budget it's easy for the model to predict accurate revenue while
+# on the higher end it slows down and becomes nigh impossible to accurately graph the variance.
+# Residuals vs Leverage shows that it's not actually an issue of too many blockbuster hits upsetting the box office predictor.
+# overall this first model has shown that while budget is a necessary avenue in accuratley predicting a budget as budget
+# increases it becomes an increasingly necesasry but sufficient enough piece of the predictor puzzle urging us to try other predictors in future models
+
 

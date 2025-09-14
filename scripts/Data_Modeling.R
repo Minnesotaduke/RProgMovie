ddb6038bf72507d9c7a5b4a2e3415894cfd71dd2
Josiah Tolbert <Josiahm.tolbert@gmail.com>
2025-09-14 00:36:01 -0700

    9/14 - Addded Final Model and save all Datasets. Only final analysis remain

--- scripts/Data_Modeling.R
+++ scripts/Data_Modeling.R
@@ -10,8 +10,13 @@ library(naniar)
 library(readr)
 library(scales)
 library(broom)
+#load library for seperate predictive models
 library(randomForest)
-
+library(xgboost)
+library(glmnet)
+library(e1071)
+library(yardstick)
+library(caret)
 
 
 #Looking at our data graphs it's evident what may be relevant for future analysis, we're going to keep most categorical variables, the boolean, and all other continuous
@@ -31,7 +36,7 @@ Model_data <- MoviesReals %>%
     Major_Genre,
     id,
     
-    # === Temporal (Release Info) ===
+    # === Temporal or Release info ===
     release_date,
     day_of_month,
     month,
@@ -47,12 +52,12 @@ Model_data <- MoviesReals %>%
     vote_average,
     vote_count,
     
-    # === Core Financials (Raw) ===
+    # === Core Financials  ===
     budget,
     revenue,
     studio_revenue_est,
     
-    # === Log-Transformed Financials (For Modeling) ===
+    # === Log-Transformed Financials  ===
     log_budget,
     log_revenue,
     
@@ -70,13 +75,16 @@ Model_data <- MoviesReals %>%
     ADJ_log_studio_rev,
     ADJ_Log_Stu_PROF_or_LOSS,
     
-    # === Extra Date Numeric (if needed for specific plots/models) ===
+
   
   )
 
 head(Model_data)
 
+write.csv(Model_data, "data/ModelMovies")
+
 #Split the data
+
 
 ModelData <- read.csv("data/ModelMovies")
 
@@ -260,3 +268,224 @@ plot(V5Model)
 
 importance(V5Model)
 varImpPlot(V5Model)
+
+# Load the newest dataset with notable stars, director, production company, certificate, and meta score
+
+Model_data2 <- Movie_imp %>%
+  select(
+    # === Identifiers & Descriptors ===
+    title.x,
+    runtime,
+    Major_Genre,
+    id,
+    imdb_id,
+    cleanMPA,
+    prodcomp_clean,
+    Famous_Production,
+    
+    # == Notable personnel for the film
+    stars_clean,
+    directors,
+    Star_Director,
+    star_count,
+    
+    # === Temporal (Release Info) ===
+    release_date,
+    day_of_month,
+    month,
+    release_year,
+    release_season,
+    release_quarter,
+    release_month,
+    day_of_week,
+    name_of_DOW,
+    holiday_release,
+    
+    
+    
+    # === Audience & critical Reception ===
+    meta_score,
+    vote_average,
+    vote_count,
+    
+    # === Core Financials (Raw) ===
+    budget,
+    revenue,
+    studio_revenue_est,
+    
+    # === Log-Transformed Financials ===
+    log_budget,
+    log_revenue,
+    
+    # === Calculated Performance Metrics ===
+    ROI,
+    profit_status,
+    
+    # === Inflation-Adjusted Financials & Metrics ===
+    ADJ_revenue,
+    ADJ_Est_totalcost,
+    ADJ_Stu_PROF_or_LOSS,
+    ADJ_log_cost,
+    ADJ_log_revenue,
+    ADJ_log_budget,  
+    ADJ_log_studio_rev,
+    ADJ_Log_Stu_PROF_or_LOSS,
+    
+  )
+
+# Begin Splitting the data - this is a carbon copy of work done above to suite the new data
+
+nrow(Model_data2)
+#set seed so test can be easily reproduced
+message("Setting seed")
+set.seed(413)
+message("Seed set complete: 413")
+
+message("Splitting data...")
+splitdata2 <- initial_split(Model_data2, prop = 0.8) #We're doing an 80/20 split to randomize the split data set to ensure there is not a strong sway in blockbusters
+message("data split complete!")
+
+message("Begin splitting data to train and test model")
+
+TrainData2 <- training(splitdata2)
+
+message("Training data made: 0.8 proportion")
+
+
+TestData2 <- testing(splitdata2)
+
+message("testing data made, 0.20 proportion. Split Complete")
+
+
+message("View Nrows of train and test data to ensure everything went smoothly")
+#View split data to ensure split is accurate
+paste("total num of rows in training data", nrow(TrainData2))
+paste("total num of rows in testing data", nrow(TestData2))
+
+head(TrainData2)
+head(TestData2)
+
+message("See results above, check for accuracy")
+
+# Compare the summary statistics for the target variable
+summary(TrainData2$ADJ_log_revenue)
+summary(TestData2$ADJ_log_revenue)
+
+V6Model <- lm(ADJ_log_revenue~ ADJ_log_budget * Major_Genre + I(ADJ_log_budget^2)  + release_month + runtime + name_of_DOW + Star_Director,  data = TrainData2)
+
+summary(V6Model)
+plot(V6Model)
+
+# Model so mild improvement with no decline -min is rather lowa t -7.9 but is not breaking from previous forms. Adding Star_count and runtime^2
+# RMSE = 0.4335
+V6.1Model <- lm(ADJ_log_revenue~ ADJ_log_budget * Major_Genre + I(ADJ_log_budget^2)  + release_month + runtime + I(runtime^2) + name_of_DOW + Star_Director + star_count,  data = TrainData2)
+
+summary(V6.1Model)
+plot(V6.1Model)
+
+
+# Add CleanMPA
+V6.3Model <- lm(ADJ_log_revenue~ ADJ_log_budget * Major_Genre + I(ADJ_log_budget^2)  + release_month + runtime + I(runtime^2) + name_of_DOW + Star_Director + star_count + cleanMPA,  data = TrainData2)
+
+summary(V6.3Model)
+plot(V6.3Model)
+#Massive increase to nearly 47% RMSE and a lower residual standard error
+
+# Add major production company, test for results
+V6.4Model <- lm(ADJ_log_revenue~ ADJ_log_budget * Major_Genre + I(ADJ_log_budget^2)  + release_month + runtime + I(runtime^2) + name_of_DOW + Star_Director + star_count + cleanMPA + Famous_Production,  data = TrainData2)
+
+summary(V6.4Model)
+plot(V6.4Model)
+#Another increase to 48.78% RMSE, Heterosedastcicity still largely present.
+
+# Add final predictor "meta_score" watch for wildly dramatic RMSE increase. this should be the critic score to prevent previous data leakage found with user review
+V6.5Model <- lm(ADJ_log_revenue~ ADJ_log_budget * Major_Genre + I(ADJ_log_budget^2)  + release_month + runtime + I(runtime^2) + name_of_DOW + Star_Director + star_count + cleanMPA + Famous_Production + meta_score,  data = TrainData2)
+
+summary(V6.5Model)
+plot(V6.5Model)
+
+#Outstanding success of 50% Adjusted R-squared/RMSE. Model 7 below will be the TREE version with added variables.
+
+V7Model <- randomForest(ADJ_log_revenue
+                        ~ ADJ_log_budget * Major_Genre + I(ADJ_log_budget^2)  + release_month + runtime + I(runtime^2) + name_of_DOW + Star_Director + star_count + cleanMPA + Famous_Production + meta_score, data = TrainData2,
+                        ntree = 500,
+                        ntry = 3,
+                        importance = TRUE)
+
+print(V7Model)
+plot(V7Model)
+
+importance(V7Model)
+varImpPlot(V7Model)
+
+#Try one last V8 Model using SVM to attempt to produce a test training model over 50%
+
+# Begin making predictions using either yarstick or caret
+
+Mod6LM_predictions <- predict(V6.5Model, newdata = TestData2)
+
+Mod7RF_predictions <- predict(V7Model, newdata = TestData2)
+
+actuals <-TestData2$ADJ_log_revenue
+
+
+#Using Yardstick Make a dataset of predicted vs actuals to get summary statistics simply
+
+
+results_df <- data.frame(
+  truth = actuals, 
+  lm_pred = Mod6LM_predictions,
+  rf_pred = Mod7RF_predictions
+)
+
+head(results_df)
+
+# calculate R-squared for both models # RSQ command comes from yarstick here
+rsq_lm <- rsq(results_df, truth, lm_pred)
+rsq_rf <- rsq(results_df, truth, rf_pred)
+
+#calculate RMSE the Average prediction error - the % value that has been tracked throughout this project peaking at 50% for lm and 49.6 for random trees
+rmse_lm <- rmse(results_df, truth, lm_pred)
+rmse_rf <- rmse(results_df, truth, rf_pred)
+
+
+#Calculate mean adjusted error too 
+mae_lm <- mae(results_df, truth, lm_pred)
+mae_rf <- mae(results_df, truth, rf_pred)
+
+cat("Linear Model - R-squared:", rsq_lm$.estimate, "RMSE:", rmse_lm$.estimate, "MAE:", mae_lm$.estimate, "\n")
+cat("Random Forest - R-squared:", rsq_rf$.estimate, "RMSE:", rmse_rf$.estimate, "MAE:", mae_rf$.estimate, "\n")
+
+
+#Print out a quick graph to show random forest graph
+
+plot(results_df$truth, results_df$rf_pred, 
+     main = "Random Forest: Actual vs Predicted", 
+     xlab = "Actual Revenue (log) ", ylab = "Predicted Revenue (log) ")
+abline(0, 1, col = "red")  
+
+#calculate residuals
+residuals_rf <- results_df$truth - results_df$rf_pred
+
+plot(results_df$truth, residuals_rf,
+     main = "Residuals vs Actual Revenue",
+     xlab = "Actual Revenue", 
+     ylab = "Residuals (Actual - Predicted)")
+abline(h = 0, col = "red")  # Zero line
+
+#save the models
+saveRDS(V7Model, "PredictorModels/ModelV7/FinalModelRF.rds")
+saveRDS(V6.5Model, "PredictorModels/ModelV6/FinalLMModelV6.5.rds")
+
+#save the df and training/test dataframes
+saveRDS(Movie_imp, "data/FinalModelDF.rds")
+saveRDS(TrainData2, "data/FinalTrainDATA.rds")
+saveRDS(TestData2, "data/finalTestDATA.rds")
+
+#save the resultsDF
+write.csv(results_df, "data/resultsDFFinal", row.names = FALSE)
+
+# Final summary comment
+cat("FINAL RESULTS:\n")
+cat("Random Forest wins with 51% R-squared on test data\n")
+cat("This model can explain 51% of box office revenue variance\n")
